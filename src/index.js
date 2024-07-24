c213c85027080a6f9363ddfffb254d23ef461bf9
Vivek Kadam <vk2567298@gmail.com>
2024-07-24 20:27:07 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
 
-    <BrowserRouter>
+    <BrowserRouter basename='Advance-Blogs'>
         <AppContextProvider>
             <App />
         </AppContextProvider>
